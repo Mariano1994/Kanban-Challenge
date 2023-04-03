ebb343729061b5a7e617fb79fed5ff9e87ca815c
Mariano <marianocapiliku@gmail.com>
2023-04-03 04:46:56 +0100

    Add functionalteis (JavaScript) to open and close

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
+// Seleting Elements
 
+const openModalButton = document.querySelector('#new-card');
+const closeModalButton = document.querySelector('.close-modal');
+const modalElement = document.querySelector('.modal');
+const overlayElement = document.querySelector('.overlay');
+
+// funtion to Open Modal
+const openModal = function () {
+  modalElement.classList.remove('hidden');
+  overlayElement.classList.remove('hidden');
+};
+
+// Function to close modal
+const closeModal = function () {
+  modalElement.classList.add('hidden');
+  overlayElement.classList.add('hidden');
+};
+
+// Opening Modal
+openModalButton.addEventListener('click', openModal);
+
+// Closing Modal clicking in X button
+closeModalButton.addEventListener('click', closeModal);
+
+// Closing Modal clicking on overlay zone
+overlayElement.addEventListener('click', closeModal);
+
+// Closing Modal Clicking in Escape(ESC) key
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modalElement.classList.contains('hidden')) {
+    closeModal();
+  }
+});
