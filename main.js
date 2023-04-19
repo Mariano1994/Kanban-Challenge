92b82440028b736d28e1855578fba5e8f061d366
Mariano <marianocapiliku@gmail.com>
2023-04-19 01:12:09 +0100

    Add Functionalities do add new cards
    Create a card constructor function, that will allow to create new cards
    whith the same instructures.
    
    Create a function to add a new card from the users information

--- main.js
+++ main.js
@@ -1,5 +1,70 @@
-// Seleting Elements
+// Selection all elements to add new Card
+const addNewCardButton = document.querySelector('.add-card-button');
+const cardTitle = document.querySelector('.card-title');
+const cardDescription = document.querySelector('.card-description');
+const entryCards = document.querySelector('.input');
 
+
+// CARD CONSTRUCTOR
+const myCards = []
+const Card = function(title, description) {
+  this.title = title;
+  this.description = description;
+}
+
+// FUNCTION TO CREATE A NEW CARD
+const createNewCard = function(title, description) {
+  const card = Object.create(Card.prototype);
+  card.title = title;
+  card.description = description;
+
+  return card;
+}
+
+// ADDING NEW CARD
+addNewCardButton.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  const newCard = createNewCard(cardTitle.value, cardDescription.value);
+  myCards.push(newCard);
+ 
+  
+  myCards.map(function(card){
+    // Creating HTML Element
+    const cardHtml = `
+    <div class="card">
+      <strong> #${card.title} 🧑🏾‍💻</strong>
+      <p>
+       ${card.description}
+     </p>
+     <div class="tag">
+      <span>rocketseat</span>
+      <span>challenge</span>
+     </div>
+   </div>`
+  
+  // Attach the html element to the cards grid
+  entryCards.insertAdjacentHTML('afterbegin', cardHtml);
+  });
+
+  // Invoke funtion to close the modal window
+  closeModal()
+
+})
+
+const clearInputs = function() {
+cardTitle.value = '';
+cardDescription.value = '';
+}
+
+
+
+
+// =============================
+// MODAL FUCNTIONALITIES
+// =============================
+
+//  Selecting all Elements
 const openModalButton = document.querySelector('#new-card');
 const closeModalButton = document.querySelector('.close-modal');
 const modalElement = document.querySelector('.modal');
@@ -12,9 +77,10 @@ const openModal = function () {
 };
 
 // Function to close modal
-const closeModal = function () {
+function closeModal() {
   modalElement.classList.add('hidden');
   overlayElement.classList.add('hidden');
+  clearInputs();
 };
 
 // Opening Modal
